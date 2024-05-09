e6fbbd6c1cbb857c1430b9294269b7e08ed649f3
Mohammed Elhasnaoui <brosimo@outlook.fr>
2024-05-09 18:13:46 -0400

    Added readme file and cleaning

--- Database/uhack2024.sql
+++ Database/uhack2024.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 27, 2024 at 10:35 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
---
+++ README.md
@@ -0,0 +1,23 @@
+# UHACK 2024
+
+
+
+#### By Mohammed Elhasnaoui, Ayman Lahdili, Dhia Naas and Hechun Ouyang
+
+
+## Project description
+
+
+## Local installation
+
+## Extensive documentation
+
+### Algorithm
+
+### Backend
+
+### Frontend
+
+### Database
+
+
